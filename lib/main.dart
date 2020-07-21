9e06fd33340417aaf5c3700e254eb242f819a09a
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-07-22 01:57:25 +0200

    getLocation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:clima/screens/loading_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(Myapp());
 }
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home:SafeArea(child: LoadingScreen())
+    );
+  }
+}
+
 
---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  Position _currentPosition;
+  getLocation() async {
+     Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+     setState(() {
+       _currentPosition=position;
+     });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [RaisedButton(
+            onPressed: (){
+              getLocation();
+            },
+            child: Text(
+              "Get location",
+            ),
+          ),
+          if(_currentPosition!=null)
+          Text("lat: ${_currentPosition.latitude} , long: ${_currentPosition.longitude}")
+          ],
+        ),
+      ),
+    );
+  }
+}
